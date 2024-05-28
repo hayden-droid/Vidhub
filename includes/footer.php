fb402ab1bf29363fe4c52dc0143de19db3d78ce6
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2024-05-28 12:16:05 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,24 @@
 	
-    <!-- <footer class="footer-area">
+    <footer class="footer-area">
         <div class="copywrite-area">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-12 col-sm-6">
                         <p class="copywrite-text">
-                        	Made by Nabil Farhan. Original template from Colorlib.com
+                        	Made by Memeitizer Limited
 						</p>
                     </div>
                     <div class="col-12 col-sm-6">
                         <nav class="footer-nav">
                             <ul>
-                                <li><a href="#">Go to Nabil's Website to check out other projects</a></li>
+                                <li><a href="https://tiktok.com/@memeitizer_">Go to Follow my tiktok</a></li>
                             </ul>
                         </nav>
                     </div>
                 </div>
             </div>
         </div>
-	</footer> -->
+</footer>
 
 	<script src="js/jquery/jquery-2.2.4.min.js"></script>
     <!-- Popper js -->
