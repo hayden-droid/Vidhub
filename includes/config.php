7229235b96ac25d06f2671fc97a6e0f77b5565ab
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2024-05-28 12:19:47 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@ date_default_timezone_set("Europe/London");
 
 try{
 	// $con = new PDO("**Your Host Name**", "**Your DB name**", "**Your DB Password**");
-	$con = new PDO("mysql:host=localhost;dbname=vidhub", 'root', 'root');
+	$con = new PDO("mysql:host=db5015879747.hosting-data.io;dbname=dbs12945197", 'dbu1516412', 'Hi9hay9den');
 	$con-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 } catch(PDOException $e){
 	echo "Connection failed: " . $e->getMessage();
